0935ec4fd2b63e5fd16cc64d54ae07c3c158d45e
doc-ock <yveltcl@gmail.com>
2021-04-08 00:15:12 -0400

    dalek works!!

--- main.c
+++ main.c
@@ -38,7 +38,7 @@ long MAXHISTSIZE = 0;
 
 // Function declarations
 char ** addtohistory(FILE * fp, char ** history, char * args);
-int background(char ** args, int * pidarr, user_t user, int i);
+int background(char ** args, int * pidarr, user_t user);
 void builtin(char * input, char ** argv, user_t user, pid_t * pid, 
                         char ** history, int histindex, FILE * fp);
 void bye(FILE * fp);
@@ -46,7 +46,7 @@ user_t cd(char ** args, user_t user);
 int cwd(user_t user);
 int copy(char * source, char * destination, user_t user);
 int copydir(char * source, char * destination, user_t user);
-int dalek(pid_t pid, pid_t * pidarr, long pidsize);
+int dalek(pid_t pid, pid_t * pidarr);
 void dalekall(int * pid);
 void echo(char ** argv);
 int exists(char * args, user_t user);
@@ -54,12 +54,12 @@ char ** inithistory(FILE * fp);
 user_t initshell(user_t user);
 int isdir(const char * path);
 int isfile(const char * path);
-int make(char * filename);
+int make(char * filename, user_t user);
 int makedir(char * dirname, user_t user);
 char ** parser(char * input);
 void recursivecopydir(char * source, char * destination, DIR * dir, 
                       struct dirent * entry, struct stat s, user_t user);
-int repeat(char ** args, int * pidarr, user_t user, int i);
+int repeat(char ** args, int * pidarr, user_t user);
 char * pathcheck(char * path, user_t user);
 int start(char ** args, user_t user);
 
@@ -117,7 +117,7 @@ char ** replay(char ** args, char ** history, int * pid) {
     histindex--;
 
     if (len > 0 && len < histindex)
-        return parser(history[histindex - len - 1]);
+        return parser(history[histindex - len + 1]);
     else 
         fprintf(stderr, RED "ERROR " RESET "mysh: number out of range\n");    
 
@@ -377,7 +377,10 @@ char * pathcheck(char * path, user_t user) {
         }
         // Place the path on the shell's current directory path.
         strcpy(newpath, user.dir);
+        strcat(newpath, "/");
         strcat(newpath, path);
+        // Free up the memory of the old path variable.
+        free(path);
         return newpath;
     }
     return path;
@@ -405,12 +408,12 @@ int start(char ** args, user_t user) {
     return 1;
 }
 
-int background(char ** args, int * pidarr, user_t user, int i)
+int background(char ** args, int * pidarr, user_t user)
 {
     // Background should be the same as start, except we don't wait. 
     // I don't really know why, but when executed the function still shows up
     // on stdout rather than through  
-    pid_t pid = pidarr[i];
+    pid_t pid = pidarr[histindex - 1];
     // check if already a daemon
     if (kill(pid, 0)) {
         fprintf(stderr, "mysh : process with pid %d already exists\n", pid);
@@ -436,23 +439,56 @@ int background(char ** args, int * pidarr, user_t user, int i)
         close(empty);
     }
     // Add pid to the pid table.
-    pidarr[i] = pid;
+    pidarr[histindex - 1] = pid;
     // Increment the history counter.
-    i++;
+    histindex++;
     printf("+ background pid: %i, command: %s\n", pid, args[0]);
-    return i;
+    return 1;
 }
 
-int dalek(pid_t pid, pid_t * pidarr, long pidsize) {
+void list(char * path, user_t user) {
+    // Wrote this because I wanted to see my directory file paths as well. 
+    // Only kind of works.
+    struct dirent *dir;
+    DIR *pdir;
+    struct stat s;
+    if (path == NULL)
+        path = strdup(user.dir);
+
+    else 
+        path = pathcheck(path, user);
+    
+    pdir = opendir(path);
+
+    if (pdir == NULL) {
+        fprintf(stderr, RED "ERROR " RESET" mysh: unable to open directory\n");
+        return;
+    }
+
+    while ((dir = readdir(pdir)) != NULL) {
+        if (!stat(dir->d_name, &s)) {
+            if (S_ISREG(s.st_mode) || S_ISDIR(s.st_mode)) { 
+                fprintf(stdout, "%s\t", dir->d_name); 
+            }
+        }
+    }
+    fprintf(stdout, "\n");
+    
+    closedir(pdir);
+}
+
+int dalek(pid_t pid, pid_t * pidarr) {
     // There should be some sort of logic to check if the pid is negative so 
     // you don't do kill -1.
     if (pid <= 0)
         pid *= -1;  // + change to positive
 
     int exists = 0;
-    for (int i = 0; i < pidsize; i++)
-        if (pidarr[i] == pid)
+    for (int i = 0; i < histindex; i++)
+        if (pidarr[i] == pid) {
             exists = 1;
+            pidarr[i] = 0;
+        }
     
     if (exists == 0) {
         fprintf(stderr, RED "ERROR " RESET "mysh : pid does not exist.\n");
@@ -465,7 +501,7 @@ int dalek(pid_t pid, pid_t * pidarr, long pidsize) {
     }
     
     // Yay! We killed a process.
-    fprintf(stderr, " + killed %d\n", pid);
+    fprintf(stderr, "+ killed %d\n", pid);
 
     return 0;
 }
@@ -473,10 +509,12 @@ int dalek(pid_t pid, pid_t * pidarr, long pidsize) {
 void dalekall(int * pid) {
     // This *should* work. 
     // Go through all pids and delete them, 
-    for (int i = 0; i < pidsize; i++)
+    fprintf(stdout, "Exterminating ...\n");
+    for (int i = 0; i < histindex; i++)
         if (pid[i] > 0)
-            dalek(pid[i], pid, pidsize);
+            dalek(pid[i], pid);
 
+    memset(pid, 0, sizeof(pid));
     return;
 }
 
@@ -515,35 +553,50 @@ int copy(char * source, char * destination, user_t user) {
 
     fclose(src);
     fclose(dest);
+
+    free(source);
+    free(destination);
     return 1;
 }
 
 int rm(char * file, user_t user)
 {
     FILE * fp;
-    char * buf = (char *)malloc(MAXLETTERS * sizeof(char));
+    file = pathcheck(file, user);
 
-    realpath(file, buf);
     // Check if the file exists
-    if (!(fp = fopen(buf, "r+"))) {
+    if (!(fp = fopen(file, "r+"))) {
         fprintf(stderr, RED "ERROR:" RESET "mysh: file cannot be accessed.\n");
         fprintf(stderr, "Check if you have write access in this directory.\n");
+        free(file);
         return 0;
     }
     else {
         remove(file);
     }
+    free(file);
     return 1;
 }
 
-int make(char * filename) {
+int make(char * filename, user_t user) {
     FILE * fp;
+    pathcheck(filename, user);
 
-    if (!(fp = fopen(filename, "w"))) {
-        fprintf(stderr, RED "ERROR" RESET "mysh: file could not be created\n");
+    // Check if file already exists first
+    if (isfile(filename))
+    {
+        fprintf(stderr, RED "ERROR " RESET "mysh: file already exists.\n");
         return 0;
     }
 
+    // Just in case there's anything wrong with opening the file to begin with.
+    if (!(fp = fopen(filename, "w"))) {
+        fprintf(stderr, RED "ERROR " RESET "mysh: file could not be created\n");
+        return 0;
+    }
+
+    // Project requirements
+    fprintf(fp, "Draft\n");
     fclose(fp);
     return 1;
 }
@@ -563,32 +616,30 @@ int makedir(char * dirname, user_t user) {
         fprintf(stderr, RED "ERROR " RESET "mysh: cannot create directory\n");
         return 0;
     }
+    free(dirname);
 }
 
 int isdir(const char * path) {
     struct stat s;
-    char * buffer = (char *)malloc(sizeof(char) * MAXLETTERS);
-    stat(realpath(path, buffer), &s);
-    free(buffer);
+    stat(path, &s);
     return S_ISDIR(s.st_mode);
 }
 
 int isfile(const char * path) {
     struct stat s;
-    char * buffer = (char *)malloc(sizeof(char) * MAXLETTERS);
-    stat(realpath(path, buffer), &s);
-    free(buffer);
+    stat(path, &s);
     return S_ISREG(s.st_mode); 
 }
 
 int exists(char * args, user_t user) {
+    args = pathcheck(args, user);
     // Return 2 if it is a directory
-    if (isdir(pathcheck(args, user))) {
+    if (isdir(args)) {
         fprintf(stdout, "Abode is.\n");
         return 2;
     }
     // Return 1 if it is a file
-    else if (isfile(pathcheck(args, user))) {
+    else if (isfile(args)) {
         fprintf(stdout, "Dwelt indeed.\n");
         return 1;
     }
@@ -597,16 +648,8 @@ int exists(char * args, user_t user) {
         fprintf(stdout, "Dwelt not.\n");
         return 0;
     }
+    free(args);
 }
-
-// void list(char ** args) {
-//     struct dirent dir;
-//     DIR *pdir;
-//     struct stat s;
-    
-//     pdir 
-//     return;
-// }
 
 int copydir(char * source, char * destination, user_t user) {
     DIR * dir;
@@ -657,8 +700,8 @@ void recursivecopydir(char * source, char * destination, DIR * dir,
 
                     recursivecopydir(path, outpath, dir, entry, s, user);
                 }
-                else if (isfile(path))
-                {
+
+                else if (isfile(path)) {
                     // copy file. The only issue is when recursively copying, 
                     // one must make sure that the directory exists.
                     outpath = strdup(destination);
@@ -678,18 +721,21 @@ void echo(char ** argv) {
     return;
 }
 
+int repeat(char ** args, int * pidarr, user_t user) {
+    long num;
+    char * ptr;
 
-int repeat(char ** args, int * pidarr, user_t user, int i) {
-    int num;
-    char ** tmp = args;
-    if (!(num = atoi(args[0]))) {
+    if (!(num = strtol(args[1], &ptr, DECIMAL))) {
         fprintf(stderr, "mysh: first argument must be a number\n");
         return 0;
     }
-    tmp++;
+
+    char ** tmp = args;
+    tmp += 2;
+
     while (num > 0) {
-        i = background(tmp, pidarr, user, i);
-        num++;
+        background(tmp, pidarr, user);
+        num--;
     }
     return 1;
 }
@@ -712,18 +758,27 @@ void builtin(char * input, char ** argv, user_t user, pid_t * pid,
         tmp = NULL;
     }
 
+    else if (!strcmp(argv[0], "list")) {
+        tmp++;
+        list(tmp[0], user);
+        tmp = NULL;
+    }
+
     else if (!strcmp(argv[0], "clear"))
         clear();
         
     else if (!strcmp(argv[0], "dalek")) {
         char ** ptr = NULL;
         if (argv[1] == NULL) {
-            fprintf(stderr, RED "ERROR " RESET "mysh : argument required for dalek\n");
+            fprintf(stderr, RED "ERROR " RESET "mysh : PID required for dalek\n");
             return;
         }
-        dalek(strtol(argv[1], ptr, DECIMAL), pid, pidsize);
+        dalek(strtol(argv[1], ptr, DECIMAL), pid);
         pid[histindex] = 0;
     }
+
+    else if (!strcmp(argv[0], "dalekall"))
+        dalekall(pid);
         
     else if (!strcmp(argv[0], "echo")) {
         tmp++;
@@ -731,9 +786,14 @@ void builtin(char * input, char ** argv, user_t user, pid_t * pid,
         tmp = NULL;
     }
 
+    else if (!strcmp(argv[0], "repeat"))
+    {
+        repeat(argv, pid, user);
+    }
+
     else if (!strcmp(argv[0], "background")) {
         tmp++;
-        background(tmp, pid, user, histindex);
+        background(tmp, pid, user);
         tmp = NULL;
     }
 
@@ -742,7 +802,7 @@ void builtin(char * input, char ** argv, user_t user, pid_t * pid,
     }
 
     else if (!strcmp(argv[0], "maik")) {
-        make(argv[1]);
+        make(argv[1], user);
     }
 
     else if (!strcmp(argv[0], "maikdir")) {
@@ -781,8 +841,8 @@ int main() {
     char ** history = inithistory(fp);
     pid_t pid[MAX_INPUT] = { 0 };
 
-    while(1)
-    {
+    while(1) {
+
         setbuf(stdout, NULL);
         prompt(user);
         // process whatever is in the buffer
@@ -811,6 +871,7 @@ int main() {
         free(input);
         free(argv);
         fflush(stdout);
+
     }
 
     return 0;
